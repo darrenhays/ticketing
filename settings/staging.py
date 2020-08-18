ada34ef88decc4069083c878173d3282ea505217
Darren Hays <darrenhaysdesign@gmail.com>
2020-08-18 10:34:42 -0500

    AbstractObject, Ticket, User update

--- settings/staging.py
+++ settings/staging.py
@@ -3,7 +3,9 @@ from secrets.secrets import get_secret
 
 BASE_URL = 'https://kg3bhyded2.execute-api.us-east-2.amazonaws.com/staging'
 EVENTS_TABLE_NAME = 'events_staging'
+PURCHASES_TABLE_NAME = 'purchases_staging'
 SALT = get_secret('SALT').get('staging')
-SESSIONS_TABLE_NAME = "sessions_staging"
+SESSIONS_TABLE_NAME = 'sessions_staging'
+TICKETS_TABLE_NAME = 'tickets_staging'
 TICKET_TYPES_TABLE_NAME = 'ticket_types_staging'
-USERS_TABLE_NAME = "users_staging"
+USERS_TABLE_NAME = 'users_staging'
