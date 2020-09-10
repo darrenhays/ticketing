12a2491c66d0795fc599913926fb0ed79fcbe376
Darren Hays <darrenhaysdesign@gmail.com>
2020-09-10 12:29:45 -0500

    update secrets manager

--- settings/production.py
+++ settings/production.py
@@ -4,7 +4,7 @@ from secrets.secrets import get_secret
 BASE_URL = 'https://puxbeuecqd.execute-api.us-east-2.amazonaws.com/production'
 EVENTS_TABLE_NAME = 'events_production'
 PURCHASES_TABLE_NAME = 'purchases_production'
-SALT = get_secret('SALT').get('production')
+SALT = get_secret('SECRETS').get('production_salt')
 SESSIONS_TABLE_NAME = 'sessions_production'
 TICKETS_TABLE_NAME = 'tickets_production'
 TICKET_TYPES_TABLE_NAME = 'ticket_types_production'
--- settings/staging.py
+++ settings/staging.py
@@ -4,7 +4,7 @@ from secrets.secrets import get_secret
 BASE_URL = 'https://kg3bhyded2.execute-api.us-east-2.amazonaws.com/staging'
 EVENTS_TABLE_NAME = 'events_staging'
 PURCHASES_TABLE_NAME = 'purchases_staging'
-SALT = get_secret('SALT').get('staging')
+SALT = get_secret('SECRETS').get('staging_salt')
 SESSIONS_TABLE_NAME = 'sessions_staging'
 TICKETS_TABLE_NAME = 'tickets_staging'
 TICKET_TYPES_TABLE_NAME = 'ticket_types_staging'
