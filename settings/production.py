6528c5ebe08d263f7dc28823ab5e784ade6dbec9
Darren Hays <darrenhaysdesign@gmail.com>
2020-09-10 16:07:31 -0500

    updated payment handler methods

--- settings/production.py
+++ settings/production.py
@@ -1,3 +1,4 @@
+import stripe
 from secrets.secrets import get_secret
 
 
@@ -9,3 +10,6 @@ SESSIONS_TABLE_NAME = 'sessions_production'
 TICKETS_TABLE_NAME = 'tickets_production'
 TICKET_TYPES_TABLE_NAME = 'ticket_types_production'
 USERS_TABLE_NAME = 'users_production'
+STRIPE_SECRET_API_KEY = get_secret('SECRETS').get('stripe_api_key')
+STRIPE_PUBLIC_API_KEY = "pk_test_51HPtHoCZ4yfyFkHo8VsiFZtp5vbuCsRyD7qLTlUf1Gqp5PwmmE4xnBTc7QlwwBOnm4Wp0y2HIcVi6316eLUp5QrF00BNgrSrBN"
+stripe.api_key = STRIPE_SECRET_API_KEY
